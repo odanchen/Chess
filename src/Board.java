29e3220abae698aac2c096a44a701dc950b1f15d
dan cooper <88119677+dancoopper@users.noreply.github.com>
2022-12-10 18:35:34 -0500

    add's the old code temp code for the board class

--- src/Board.java
+++ src/Board.java
@@ -4,6 +4,31 @@ import java.util.List;
 
 public class Board {
     ColorPair boardColor = BoardColors.OPTION1;
-
     List<List<ChessPiece>> configuration;
+    
+     public static void main(String[] args) {//old code (not using ColorPair class) 
+        JFrame frame = new JFrame();
+        frame.setBounds(10,10,528,550);
+        JPanel pn = new JPanel(){
+            @Override
+            public void paint(Graphics g){
+                boolean white = true;
+                for(int y = 0; y<8; y++){
+                    for(int x = 0; x<8; x++){
+                        if(white){
+                            g.setColor(Color.lightGray);
+                        }else {
+                            g.setColor(Color.darkGray);
+                        }
+                        g.fillRect(x*64, y*64, 64,64);
+                        white=!white;
+                    }
+                    white=!white;
+                }
+            }
+        };
+        frame.add(pn);
+        frame.setDefaultCloseOperation(3);
+        frame.setVisible(true);
+    }
 }
