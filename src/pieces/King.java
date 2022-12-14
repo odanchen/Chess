2f741f022477b8b01e2bb50a9795991bd2259ef4
odanchen <sanek.danchenko@gmail.com>
2022-12-14 12:55:58 -0500

    Added basic king move set, without castling

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -2,14 +2,42 @@ package pieces;
 
 import BoardPackage.Board;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class King extends ChessPiece {
     private boolean hasMoved;
+    private boolean canMoveTo(Position position, Board board)
+    {
+        return (position.isInsideBoard() &&
+                (board.getPieceAt(position) == null || board.getPieceAt(position).getPieceColor() != this.getPieceColor()));
+    }
     @Override
     public void calculateMoves(Board board)
     {
+        List<Position> moves = new ArrayList<>();
 
+        Position direction = new Position(this.getPosition(), 1, 0);
+        if (canMoveTo(direction, board)) moves.add(direction);
+        direction = new Position(this.getPosition(), 1, 1);
+        if (canMoveTo(direction, board)) moves.add(direction);
+
+        direction = new Position(this.getPosition(), 1, -1);
+        if (canMoveTo(direction, board)) moves.add(direction);
+        direction = new Position(this.getPosition(), -1, 0);
+        if (canMoveTo(direction, board)) moves.add(direction);
+
+        direction = new Position(this.getPosition(), -1, 1);
+        if (canMoveTo(direction, board)) moves.add(direction);
+        direction = new Position(this.getPosition(), -1, -1);
+        if (canMoveTo(direction, board)) moves.add(direction);
+
+        direction = new Position(this.getPosition(), 0, 1);
+        if (canMoveTo(direction, board)) moves.add(direction);
+        direction = new Position(this.getPosition(), 0, -1);
+        if (canMoveTo(direction, board)) moves.add(direction);
+
+        this.setMoves(moves);
     }
     public King(Position position, PieceColor color, boolean hasMoved)
     {
