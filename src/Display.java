b3e36904cb011def977ef4f7bd50ab0c5caf7bde
dan cooper <88119677+dancoopper@users.noreply.github.com>
2022-12-18 17:24:46 -0500

    the start button now calls game run and draws the board

--- src/Display.java
+++ src/Display.java
@@ -3,33 +3,30 @@ import pieces.PieceColor;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class Display {
 
     public static void MainMenu() {
 
-        JLabel Ltitle = new JLabel();
-        Ltitle.setText("Chess Game");
-        Ltitle.setFont(new Font("Serif", Font.PLAIN, 20));
+        JLabel lTitle = new JLabel();
+        lTitle.setText("Chess Game");
+        lTitle.setFont(new Font("Serif", Font.PLAIN, 20));
 
         JPanel title = new JPanel();
         title.setBounds(220, 150, 150, 50);
         //title.setBackground(Color.green);
-        title.add(Ltitle);
+        title.add(lTitle);
 
         Dimension BtnSize = new Dimension(130, 50);
 
         JButton start = new JButton("   Start   ");
 
-        start.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("You started the game");
-               GameControl gameRuner = new GameControl();
-               gameRuner.runTheGame(new Board()/*i have no clue what to put here but when i find out i will put it here :)*/, PieceColor.WHITE);
-            }
+        start.addActionListener(e -> { // uncomment the call for draw board for the start button to work
+            //drawBoard();
+            System.out.println("You started the game");
+            GameControl gameRuner = new GameControl();
+            gameRuner.runTheGame(new Board()/*I have no clue what to put here but when I find out I will put it here :)*/, PieceColor.WHITE);
         });
 
         start.setPreferredSize(BtnSize);
@@ -59,7 +56,8 @@ public class Display {
 
         JFrame f = new JFrame("Chess");
         f.setSize(600, 600);
-        f.setDefaultCloseOperation(3);
+
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         f.setLayout(null);
         f.setVisible(true);
         f.setResizable(false);
@@ -69,7 +67,70 @@ public class Display {
 
     }
 
-    public void drawBoard(){
+    public static void drawBoard(){
+
+        JFrame frame = new JFrame();
+        frame.setBounds(10,10,528,550);
+        JPanel pn = new JPanel(){
+            @Override
+            public void paint(Graphics g){
+                boolean white = true;
+                for(int y = 0; y<8; y++){
+                    for(int x = 0; x<8; x++){
+                        if(white){
+                            g.setColor(Color.lightGray);
+                        }else {
+                            g.setColor(Color.darkGray);
+                        }
+                        g.fillRect(x*64, y*64, 64,64);
+                        white=!white;
+                    }
+                    white=!white;
+                }
+                // draw piece img here
+            }
+        };
+        frame.add(pn);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        frame.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        frame.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
 
     }
 
