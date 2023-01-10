1e012bc3273273f89c1780c8f3f9b2ba5fd54a15
dan cooper <88119677+dancoopper@users.noreply.github.com>
2023-01-10 11:41:46 -0500

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,34 +1,44 @@
-import pieces.ChessPiece;
+package root.ui.frames.menu_frame;
 
-import java.util.List;
+import javax.swing.*;
+import java.awt.*;
 
-public class Board {
-    ColorPair boardColor = BoardColors.OPTION1;
-    List<List<ChessPiece>> configuration;
-    
-     public static void main(String[] args) {//old code (not using ColorPair class) 
-        JFrame frame = new JFrame();
-        frame.setBounds(10,10,528,550);
-        JPanel pn = new JPanel(){
-            @Override
-            public void paint(Graphics g){
-                boolean white = true;
-                for(int y = 0; y<8; y++){
-                    for(int x = 0; x<8; x++){
-                        if(white){
-                            g.setColor(Color.lightGray);
-                        }else {
-                            g.setColor(Color.darkGray);
-                        }
-                        g.fillRect(x*64, y*64, 64,64);
-                        white=!white;
-                    }
-                    white=!white;
-                }
-            }
-        };
-        frame.add(pn);
-        frame.setDefaultCloseOperation(3);
-        frame.setVisible(true);
+import root.ui.GameManager;
+import root.ui.frames.BaseFrame;
+import root.ui.frames.menu_frame.panels.BackgroundPanel;
+import root.ui.graphics.GraphicsManager;
+public class SettingsFrame extends BaseFrame {
+    public SettingsFrame(GameManager gameManager, GraphicsManager graphicsManager) {
+        super(gameManager, graphicsManager);
+
+        JPanel panel = new JPanel();
+        panel.setBounds(graphicsManager.getGameBounds().height/4,graphicsManager.getGameBounds().width/4,graphicsManager.getGameBounds().width/2,170);
+        //panel.setBackground(Color.blue);
+        JLabel lbl = new JLabel("Select one of the possible choices and click OK");
+        lbl.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        panel.add(lbl);
+
+        String[] choices = { "Cburnett", "Kilifiger", "Kosal", "Legipzig",
+                "Maya", "Pirat", "Regular" };
+
+
+
+        final JComboBox<String> cb = new JComboBox<>(choices);
+        cb.setSelectedItem(choices[2]);
+        cb.setMaximumSize(cb.getPreferredSize());
+        cb.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(cb);
+
+        JButton btn = new JButton("OK");
+        btn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(btn);
+        setVisible(true);
+
+
+
+        add(panel);
+
     }
 }
+
