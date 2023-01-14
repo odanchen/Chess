fdfdb69e05dd98731f116ecfb077274e760cf54c
Parsa Hemmati <parsahemmati20@gmail.com>
2023-01-13 22:44:55 -0500

    Fixed a little bug

--- src/root/ui/frames/game_frame/panels/GamePanel.java
+++ src/root/ui/frames/game_frame/panels/GamePanel.java
@@ -260,9 +260,9 @@ public class GamePanel extends JPanel {
 
     private boolean isActionDeselect(MouseEvent e) {
         if (isClickOutsideBoard(e)) return true;
-        if (gameStatus.getPieceAt(getPositionOnTheBoard(e)) == null) return true;
+        if (gameStatus.getPieceAt(getPositionOnTheBoard(e)) == null && !isActionMove(e)) return true;
         if (!gameStatus.isPieceSelected()) return false;
-        if (gameStatus.getPieceAt(getPositionOnTheBoard(e)).equals(gameStatus.getSelectedPiece())) return false;
+        if (gameStatus.getPieceAt(getPositionOnTheBoard(e)) != null && gameStatus.getPieceAt(getPositionOnTheBoard(e)).equals(gameStatus.getSelectedPiece())) return false;
         return getMoveOnClick(e) == null;
     }
 
