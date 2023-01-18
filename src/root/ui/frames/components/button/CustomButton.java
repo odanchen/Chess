f795b12ea08ca34a9a5ff6d7bfd1a965a955c937
Parsa Hemmati <parsahemmati20@gmail.com>
2023-01-18 16:13:52 -0500

    haha.

--- src/root/ui/frames/components/button/CustomButton.java
+++ src/root/ui/frames/components/button/CustomButton.java
@@ -2,11 +2,13 @@ package root.ui.frames.components.button;
 
 import root.ui.graphics.GraphicsManager;
 
-import javax.swing.JButton;
-import javax.swing.BorderFactory;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.RescaleOp;
 
 public class CustomButton extends JButton {
 
@@ -17,6 +19,25 @@ public class CustomButton extends JButton {
         setPreferredSize(size);
         setBorderPainted(false);
         setFocusPainted(false);
-        addMouseListener(new ButtonListener(this, graphicsManager.getButtonIcon(id, size)));
+        addPhases(graphicsManager.getButtonIcon(id, size));
     }
+    private void addPhases(ImageIcon image) {
+        setRolloverIcon(darken(image));
+        setPressedIcon(darken(darken(image)));
+    }
+
+    public static Icon darken(Icon icon) {
+        BufferedImage img = getBufferedImageFromIcon(icon);
+        BufferedImageOp op = new RescaleOp(0.9f, 0, null);
+        return new ImageIcon(op.filter(img, null));
+    }
+    public static BufferedImage getBufferedImageFromIcon(Icon icon) {
+        BufferedImage buffer = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(),
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics g = buffer.getGraphics();
+        icon.paintIcon(new JLabel(), g, 0, 0);
+        g.dispose();
+        return buffer;
+    }
+
 }
