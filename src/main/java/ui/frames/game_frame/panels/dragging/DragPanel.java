bd2e9d7a18958129af897e802fb9d606bde36c84
Parsa Hemmati <parsahemmati20@gmail.com>
2023-01-21 11:48:16 -0500

    done

--- src/main/java/ui/frames/game_frame/panels/dragging/DragPanel.java
+++ src/main/java/ui/frames/game_frame/panels/dragging/DragPanel.java
@@ -7,41 +7,28 @@ import ui.graphics.GraphicsManager;
 import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 
 public class DragPanel extends JPanel {
     private final GraphicsManager graphicsManager;
     private final GameStatus gameStatus;
-    private MouseEvent mouse;
-    private final JPanel piecePanel;
+    private final PieceDraggedPanel piecePanel;
 
     public DragPanel(GraphicsManager graphicsManager, GameStatus gameStatus) {
         this.graphicsManager = graphicsManager;
         this.gameStatus = gameStatus;
         this.setBounds(graphicsManager.getPlayAreaBounds());
+        this.setVisible(true);
         this.setOpaque(false);
         this.piecePanel = new PieceDraggedPanel(graphicsManager,gameStatus);
-    }
-
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        if (mouse != null && gameStatus.getSelectedPiece() != null) drawPiece(mouse);
-    }
-
-    private void drawPiece(MouseEvent e) {
-        int row = e.getY() - graphicsManager.getSquareSize();
-        int col = e.getX() - graphicsManager.getSquareSize();
-        piecePanel.setBounds(col,row,graphicsManager.getSquareSize(),graphicsManager.getSquareSize());
+        add(piecePanel);
     }
 
     public void onNewMouseEvent(MouseEvent e) {
-        mouse = e;
-        repaint();
+        piecePanel.pieceMoved(e);
     }
 
     public void mouseReleased() {
-        mouse = null;
-        repaint();
+        piecePanel.clean();
     }
+
 }
--- src/main/java/ui/frames/game_frame/panels/dragging/PieceDraggedPanel.java
+++ src/main/java/ui/frames/game_frame/panels/dragging/PieceDraggedPanel.java
@@ -5,6 +5,7 @@ import ui.graphics.GraphicsManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 public class PieceDraggedPanel extends JPanel {
@@ -15,12 +16,27 @@ public class PieceDraggedPanel extends JPanel {
     public PieceDraggedPanel(GraphicsManager graphicsManager, GameStatus gameStatus) {
         this.graphicsManager = graphicsManager;
         this.gameStatus = gameStatus;
+        this.setBounds(0,0,graphicsManager.getSquareSize(),graphicsManager.getSquareSize());
+        this.setOpaque(false);
+        this.setVisible(true);
+    }
+
+    public void pieceMoved(MouseEvent e) {
+        setVisible(true);
+        setBounds(e.getX() - graphicsManager.getSquareSize(),e.getY() - graphicsManager.getSquareSize(),graphicsManager.getSquareSize(),graphicsManager.getSquareSize());
+    }
+
+    public void clean() {
+        setVisible(false);
     }
 
     @Override
     public void paint(Graphics g) {
-        BufferedImage image = graphicsManager.getTextureOfPiece(gameStatus.getSelectedPiece());
-        g.drawImage(image, 0, 0, null);
         super.paint(g);
+        if (gameStatus.isPieceSelected()) {
+            BufferedImage image = graphicsManager.getTextureOfPiece(gameStatus.getSelectedPiece());
+            g.drawImage(image, 0, 0, null);
+        }
     }
+
 }
