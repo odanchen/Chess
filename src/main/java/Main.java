7d0bef702a99d507726985768dad1d9570daf5df
Parsa Hemmati <parsahemmati20@gmail.com>
2023-01-22 11:13:11 -0500

    made sound on moves

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import ui.GameManager;
 
 public class Main {
     public static void main(String[] args) {
-        GameManager manager = new GameManager();
-        manager.runMenu();
+        new GameManager().runMenu();
     }
 }
